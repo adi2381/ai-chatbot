5d85813bbee6415013df583766e101f394a70d19
Aditya Kumar <adi2381@gmail.com>
2019-10-07 03:53:28 +0530

    Update chatbot.py
    moving closer to start training the model

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,7 @@ import numpy as np
 import tensorflow as tf
 import re
 import time
-
+import keras
 ### Phase 1: Data Preprocessing ###
 
 # Importing the dataset
@@ -143,9 +143,9 @@ for length in range(1, 25):
 def model_inputs():
     inputs = tf.placeholder(tf.int32, [None, None], name = 'input')
     targets = tf.placeholder(tf.int32, [None, None], name = 'input')
-    learning_rate = tf.placeholder(tf.float32, name = 'Learning_rate')
+    lr = tf.placeholder(tf.float32, name = 'Learning_rate')
     keep_prob = tf.placeholder(tf.float32, name = 'Keep_prob')
-    return inputs, targets, learning_rate, keep_prob
+    return inputs, targets, lr, keep_prob
 
 # Preprocessing the targets
 def preprocess_targets(targets, word_to_int, batch_size):
@@ -264,4 +264,43 @@ def seq2seq_model(inputs, targets, keep_prob, batch_size, sequence_length, answe
     return training_predictions, test_predictions  
 
 ### Phase 3: Training the SEQ2SEQ Model ###
-    
+
+# Setting the Hyperparameters
+epochs = 100
+batch_size = 64
+rnn_size = 512
+num_layers = 3
+encoding_embedding_size = 512
+decoding_embedding_size = 512
+learning_rate = 0.01
+learning_rate_decay = 0.9
+min_learning_rate = 0.0001
+keep_probability = 0.5
+ 
+# 
+# Defining a session
+tf.reset_default_graph()
+session = tf.InteractiveSession()
+
+# Loading the model inputs
+inputs, targets, lr, keep_prob = model_inputs()
+
+# Setting the sequence length
+sequence_length = tf.placeholder_with_default(25, None, name = 'sequence_length')
+
+# Getting the shape of the inputs tensor
+input_shape = tf.shape(inputs)
+
+# Getting the training and test predictions
+training_predictions, test_predictions = seq2seq_model(tf.reverse(inputs, [-1]),
+                                                       targets,
+                                                       keep_prob,
+                                                       batch_size,
+                                                       sequence_length,
+                                                       len(ans_words_to_int),
+                                                       len(ques_words_to_int),
+                                                       encoding_embedding_size,
+                                                       decoding_embedding_size,
+                                                       rnn_size,
+                                                       num_layers,
+                                                       ques_words_to_int)
